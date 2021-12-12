fe9ca513a574b120b35fb7140b42b6c3482ac7c7
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-12-12 14:09:32 +0200

    2.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import HelloWorld from "./components/2.1/HelloWorld.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <HelloWorld />;
 }
 
 export default App;
---
+++ src/components/2.1/HelloWorld.jsx
@@ -0,0 +1,10 @@
+function HelloWorld() {
+  return (
+    <div>
+      <h1>Appleseeds React App</h1>
+      <a href="./index.html">Click me</a>
+    </div>
+  );
+}
+
+export default HelloWorld;
