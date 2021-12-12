b36fb125eb87d6655465c9e9b6f56d4ab84d15c1
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-12-12 21:23:53 +0200

    3.1

---
+++ src/components/boxes/Boxes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Box1 from "./box1.jsx";
+
+function Boxes() {
+  return (
+    <div style={{ width: "50%" }}>
+      <Box1 />
+    </div>
+  );
+}
+
+export default Boxes;
